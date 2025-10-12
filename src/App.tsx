c0168db18cbac5f1a881c6e2fc3d58771e8be486
ivandax <ivandax_89@hotmail.com>
2025-10-12 18:51:55 +0200

    Add first shad cn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
-    return <>Hello world</>
+    return (
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <h1 className="text-3xl font-bold text-blue-600">
+                Tailwind is working 🎉
+            </h1>
+            <Button>Click me</Button>
+        </div>
+    )
 }
 
 export default App
