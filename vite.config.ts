ad1a55382b779b7c5e1e14967c693f428aa2cd1d
ivandax <ivandax_89@hotmail.com>
2025-10-12 18:46:49 +0200

    Add shadcn setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import eslint from "vite-plugin-eslint";
-import checker from "vite-plugin-checker";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import eslint from 'vite-plugin-eslint'
+import checker from 'vite-plugin-checker'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), eslint(), checker({ typescript: true })],
-});
+    plugins: [react(), tailwindcss(), eslint(), checker({ typescript: true })],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+})
